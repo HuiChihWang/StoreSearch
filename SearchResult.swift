1b1168284e7cf4b211e9bd478374b7efa0de581c
Gilbert <taya87136@.gmail.com>
2021-04-20 12:36:39 +0800

    add simple test ui and fake data source

---
+++ SearchResult.swift
@@ -0,0 +1,43 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Hui Chih Wang on 2021/4/20.
+//
+
+import Foundation
+
+struct SearchResult {
+    var name = "name"
+    var artistName = "artist"
+}
+
+class SearchModel {
+    private var results = [SearchResult]()
+    
+    var resultsNum: Int {
+        results.count
+    }
+    
+    public func search(with text: String) {
+        results = [SearchResult]()
+        
+        if (text != "0000") {
+            (0..<5).forEach { index in
+                var searchResult = SearchResult()
+                searchResult.name = "fake data \(index)"
+                searchResult.artistName = text
+                results.append(searchResult)
+            }
+        }
+    }
+    
+    public func getResult(by index: Int) -> SearchResult? {
+        guard (0..<resultsNum).contains(index) else {
+            return nil
+        }
+        
+        return results[index]
+    }
+    
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let search = SearchModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
     }
 
 
 }
+
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return search.resultsNum
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "testCell")
+        
+        let result = search.getResult(by: indexPath.row) ?? SearchResult()
+        cell.textLabel?.text = result.name
+        cell.detailTextLabel?.text = result.artistName
+        
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: '\(searchBar.text!)'")
+        
+        search.search(with: searchBar.text!)
+        tableView.reloadData()
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        .topAttached
+    }
+}
+
 
